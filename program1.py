e7b2f52fbbac96d03e60b9ea0fa341ea26989b46
poonam973 <ps2826363@gmail.com>
2024-10-07 15:12:00 +0530

    code updated

--- program1.py
+++ program1.py
@@ -4,7 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        # Dictionary to hold matching pairs
+        matching_brackets = {')': '(', '}': '{', ']': '['}
+        
+        # Stack to keep track of opening brackets
+        stack = []
+        
+        # Iterate through each character in the string
+        for char in s:
+            if char in matching_brackets:
+                # Pop the topmost element from the stack if not empty, else assign '#'
+                top_element = stack.pop() if stack else '#'
+                
+                # If the top element doesn't match the corresponding opening bracket, return False
+                if matching_brackets[char] != top_element:
+                    return False
+            else:
+                # If it's an opening bracket, push it to the stack
+                stack.append(char)
+        
+        # If the stack is empty, all brackets were matched correctly, return True
+        # Otherwise, return False
+        return not stack
 
 
 
--- program2.py
+++ program2.py
@@ -4,7 +4,30 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        # Dictionary to map Roman numerals to their respective values
+        roman_map = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        total = 0
+        prev_value = 0
+        
+        # Iterate over the characters of the Roman numeral from right to left
+        for char in reversed(s):
+            # Get the integer value of the current Roman numeral character
+            current_value = roman_map[char]
+            
+            # If the current value is less than the previous value, we subtract it
+            if current_value < prev_value:
+                total -= current_value
+            else:
+                total += current_value
+            
+            # Update the previous value to the current one for the next iteration
+            prev_value = current_value
+        
+        return total
 
 
 
